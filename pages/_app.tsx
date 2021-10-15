ffc12c200c0e6e861d27f0d2d979ed54d33a5316
Antoine Garcia <antoine.garcia@polyconseil.fr>
2021-10-15 13:00:30 +0200

    map: add mapbox and show basic map

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import "tailwindcss/tailwind.css";
+import "mapbox-gl/dist/mapbox-gl.css";
 import type { AppProps } from "next/app";
 import Layout from "../components/Layout";
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,14 @@
 import type { NextPage } from "next";
+import { WorldMap } from "../components/WorldMap";
 
 const Home: NextPage = () => {
   return (
-    <h1 className="m-8">
-      Welcome to <a href="https://nextjs.org">Next.js!</a>
-    </h1>
+    <>
+      <h1 className="m-8">
+        Welcome to <a href="https://nextjs.org">Next.js!</a>
+      </h1>
+      <WorldMap />
+    </>
   );
 };
 
