9764c3d03853e98282a23aaa003ecd967a04306a
Antoine Garcia <antoine.garcia@polyconseil.fr>
2021-10-15 18:23:06 +0200

    replace custom component with mui one

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -1,16 +1,35 @@
 import React from "react";
-import { RadioInput } from "../atoms/RadioInput";
+import Radio from "@mui/material/Radio";
+import RadioGroup from "@mui/material/RadioGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import { indigo } from "@mui/material/colors";
 import { SelectInput } from "../atoms/SelectInput";
 import { DestinationInput } from "../atoms/DestinationInput";
+
+const sx = { color: indigo[400], "&.Mui-checked": { color: indigo[400] } };
 
 export const SideBar = () => {
   return (
     <div className="absolute m-6 p-6 z-10 bg-white w-1/3 rounded-lg border-2 border-indigo-600 border-opacity-50">
       {/* Roundtrip / One-way / Multi-city */}
-      <div className="flex gap-6 mt-2 mb-10 flex-col md:flex-row center">
-        <RadioInput name="triptype" id="roundtrip" label="Roundtrip" />
-        <RadioInput name="triptype" id="oneway" label="One way" />
-        <RadioInput name="triptype" id="multicity" label="Multi city" />
+      <div className="flex gap-8 mt-2 mb-10 flex-col md:flex-row center">
+        <RadioGroup defaultValue="roundtrip" row name="trip-type-radio-buttons">
+          <FormControlLabel
+            value="roundtrip"
+            control={<Radio sx={sx} size="small" />}
+            label="Roundtrip"
+          />
+          <FormControlLabel
+            value="oneway"
+            control={<Radio sx={sx} size="small" />}
+            label="One way"
+          />
+          <FormControlLabel
+            value="multicity"
+            control={<Radio sx={sx} size="small" />}
+            label="Multi city"
+          />
+        </RadioGroup>
       </div>
 
       {/* From* / To* */}
@@ -34,12 +53,6 @@ export const SideBar = () => {
           label="Class"
           options={["Economy", "Business", "First class"]}
         />
-      </div>
-
-      <div className="flex mb-2 mt-10 justify-center mt-6">
-        <button className="bg-indigo-600 text-white font-extrabold text-lg rounded-full px-6 py-3">
-          Calculate
-        </button>
       </div>
     </div>
   );
